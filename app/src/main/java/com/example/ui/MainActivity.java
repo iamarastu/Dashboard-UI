63938045a4bf072448abcfc869977965a627ef05
Arastu Gupta <git@github.com:iamarastu/UI-dashboard.git>
2023-05-16 01:18:59 +0530

    Intial commit

---
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+}
